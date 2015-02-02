2ae6073a6cd56ba7d8676df4f96a83c02f233b61
koding user <adam@abemedia.co.uk>
2015-02-02 13:17:25 +0000

    squash! update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,4 @@ layout: nil
 {% include assets/js/jquery.min.js %}
 {% include assets/js/bootstrap.min.js %}
 {% include assets/js/ekko-lightbox.min.js %}
-{% include assets/js/fittext.js %}
-{% include assets/js/parallax.js %}
 {% include assets/js/app.js %}
