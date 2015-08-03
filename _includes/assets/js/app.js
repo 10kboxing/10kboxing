d74de8d4c361fba3798038e5073fd166c8c6f581
Adam Bouqdib <adam@abemedia.co.uk>
2015-08-03 09:14:23 +0000

    squash! Update _events/fresh-blood.html

--- _includes/assets/js/app.js
+++ _includes/assets/js/app.js
@@ -105,3 +105,63 @@ $( "#contactForm" ).submit(function( event ) {
             $form.prepend('<div class="alert alert-danger">Error: ' + data.error + '</div>');
         });
 });
+
+simpleCart({
+    checkout: {
+        type: "PayPal",
+        email: "badytouray@gmail.com"
+    },
+    currency: "GBP",
+    cartColumns: [
+        { view: function(item, column){
+            return  '<p>' + item.get('name') + '<br>' +
+                    '<a href="javascript:;" class="simpleCart_remove">Remove</a>';
+          }, attr: "name", label: "Name"
+        } ,
+        { attr: "price" , label: "Price", view: 'currency' } ,
+        { view: function(item, column){
+            return  '<a href="javascript:;" class="simpleCart_decrement">-</a> ' +
+                    item.quantity() + ' ' +
+                    '<a href="javascript:;" class="simpleCart_increment">+</a>';
+          }, attr: "custom", label: "Quantity"
+        }
+    ]
+});
+simpleCart.bind( "afterAdd" , function( item ) {
+    toastr.success( item.get("name") + " - " + item.get("size") + " was added to the basket." );
+    
+    setTimeout(function() {
+        var quantity = simpleCart.quantity();
+        toastr.info( 
+            "Your new sub-total is " + simpleCart.toCurrency( simpleCart.total() ) + " (" + quantity + " item" + (quantity !== 1 ? "s" : "") + "). &nbsp; " +
+            "<a class='btn btn-primary' href='" + $('.navbar-cart a.btn').attr('href') + "'>" +
+            "<span class='fa fa-shopping-cart'></span> View basket</a>"
+        );
+    }, 2000);
+});
+simpleCart.bind( "update", function(e) {
+    $el = $('.navbar-cart');
+    $btn = $el.find('.btn');
+    $btn.css('transition', '1s ease all');
+    if(simpleCart.quantity()) {
+        $('.simpleCart_checkout').attr('disabled', false);
+        $el.fadeIn('1000');
+        $btn.addClass('btn-primary');
+        setTimeout(function() {
+            $btn.removeClass('btn-primary');
+        }, 1000);
+    }
+    else {
+        $('.simpleCart_checkout').attr('disabled', true);
+        $('.navbar-cart').fadeOut('slow');
+    }
+});
+$('.item_size').on('change', function() {
+    $el = $('.item_size option:selected');
+    $('.item_price span').text($el.data('price'));
+    $('.poster-size').text($el.data('size'));
+});
+function get_cart_count() {
+    var quantity = simpleCart.quantity();
+    return quantity + " item" + quantity !== 1 ? "s" : "";
+}
